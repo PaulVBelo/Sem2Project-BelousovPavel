0df6c92dc4eb9716f6d971f57ecd23ee0ab230d0
PaulVBelo <144692494+PaulVBelo@users.noreply.github.com>
2024-05-20 23:50:46 +0300

    Lil'test fix

--- auction-hosting-service/src/test/java/com/example/demo2/Demo2ApplicationTests.java
+++ auction-hosting-service/src/test/java/com/example/demo2/Demo2ApplicationTests.java
@@ -1,10 +1,28 @@
 package com.example.demo2;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @SpringBootTest
 class Demo2ApplicationTests {
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:latest");
+
+	@BeforeAll
+	static void databaseOn() {
+		POSTGRES.start();
+	}
+
+	@AfterAll
+	static void databaseOff() {
+		POSTGRES.stop();
+	}
 
 	@Test
 	void contextLoads() {
